92bd04c1d203c14024e579867010f52f60d225c5
qile <98936712+csdropout@users.noreply.github.com>
2026-06-04 17:19:19 +0800

    Add basic home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,37 @@
+import styles from "./home.module.css";
+import { Link } from "react-router";
+
 function Home() {
-  return <p>This is the home page.</p>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.intro}>
+        <h1>Best Imitations</h1>
+        <p>
+          Welcome to Best Imitations where we curate a wide range of products
+          just like the real ones! Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quam possimus minus fugiat similique modi laudantium
+          reprehenderit harum est veniam distinctio optio, consequatur
+          molestiae. Ullam dolorum blanditiis sequi ipsam unde at.
+        </p>
+        <button className={styles.underlineButton}>
+          <Link to="shop">View collection</Link>
+        </button>
+      </div>
+      <Newsletter />
+    </div>
+  );
+}
+
+function Newsletter() {
+  return (
+    <div className={styles.newsletterContainer}>
+      <h2>Stay updated!</h2>
+      <div className={styles.newsletterField}>
+        <input type="email" placeholder="Email" />
+        <button>&gt;</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
