c030d96ce073e1d612c1d77309fb6f7b5464f25e
qile <98936712+csdropout@users.noreply.github.com>
2026-06-05 14:53:27 +0800

    create ItemCard component

---
+++ src/components/Shop/ItemCard.jsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react";
+import { CartContext } from "../../CartContext";
+import styles from "./itemcard.module.css";
+
+function ItemCard({ id, title, price, imageUrl }) {
+  const [quantity, setQuantity] = useState(1);
+  const cartContext = useContext(CartContext);
+
+  function addToCart() {
+    cartContext.addToCart({ id, price, quantity });
+  }
+
+  return (
+    <div className="card">
+      <img src={imageUrl} alt="Image" />
+      <p>{title}</p>
+      <p>{price}</p>
+      <div>
+        <button onClick={() => setQuantity(Math.max(1, quantity - 1))}>
+          -
+        </button>
+        <input
+          type="number"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+        <button onClick={() => setQuantity(quantity + 1)}>+</button>
+      </div>
+      <button onClick={() => addToCart()}>Add to Cart</button>
+    </div>
+  );
+}
+
+export default ItemCard;
