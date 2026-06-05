fbd4a618971d4120286881e2c67b865b65caeea3
qile <98936712+csdropout@users.noreply.github.com>
2026-06-05 21:48:59 +0800

    style: align heading

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
 import ShopGrid from "../components/Shop/ShopGrid.jsx";
+import styles from "./shop.module.css";
 
 function Shop() {
   return (
     <div className="container">
-      <h1>Products</h1>
+      <h1 className={styles.heading}>Products</h1>
       <ShopGrid />
     </div>
   );
