346923b870a7bae717a4fbe16884fa8ce50473b5
qile <98936712+csdropout@users.noreply.github.com>
2026-06-05 17:37:55 +0800

    Display items on Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,28 @@
+import { useContext } from "react";
+import { CartContext } from "../CartContext.jsx";
+import CartItemCard from "../components/Cart/CartItemCard.jsx";
+import styles from "./cart.module.css";
+
 function Cart() {
-  return <p>This is the cart page</p>;
+  const cartContext = useContext(CartContext);
+  const cart = cartContext.cart;
+
+  return (
+    <div>
+      <h1 className={styles.cartHeader}>Shopping Cart</h1>
+      <div className={styles.itemsContainer}>
+        {cart.map((item) => (
+          <CartItemCard
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            imageUrl={item.imageUrl}
+            quantity={item.quantity}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
