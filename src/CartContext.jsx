fac628ff0bb2f45d880f5a6dff14c2f01d3aa07c
qile <98936712+csdropout@users.noreply.github.com>
2026-06-05 14:52:44 +0800

    addToCart checks for existing item

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -6,30 +6,17 @@ function CartContextProvider({ children }) {
   const [cart, setCart] = useState([]);
 
   const addToCart = (item) => {
-    setCart([...cart, item]);
+    const itemInCart = cart.find((x) => x.id === item.id);
+    if (itemInCart) {
+      setCart([
+        ...cart,
+        { ...itemInCart, quantity: itemInCart.quantity + item.quantity },
+      ]);
+    } else setCart([...cart, item]);
   };
 
   const removeFromCart = (id) => {
     setCart((prev) => prev.filter((item) => item.id !== id));
-  };
-
-  const increaseQty = (id) => {
-    setCart((prev) =>
-      prev.map((item) =>
-        item.id === id ? { ...item, quantity: item.quantity + 1 } : item,
-      ),
-    );
-  };
-
-  const decreaseQty = (id) => {
-    setCart((prev) =>
-      prev.map((item) => {
-        if (item.id === id) {
-          let selectedItem = { ...item, quantity: item.quantity - 1 };
-          if (selectedItem.quantity > 0) return selectedItem;
-        } else return item;
-      }),
-    );
   };
 
   const getCount = () => {
@@ -46,8 +33,6 @@ function CartContextProvider({ children }) {
         cart,
         addToCart,
         removeFromCart,
-        increaseQty,
-        decreaseQty,
         getCount,
       }}
     >
