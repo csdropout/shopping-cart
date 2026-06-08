89ac7c669baa0ad8d438c0cfc814e0552cf37172
qile <98936712+csdropout@users.noreply.github.com>
2026-06-08 15:40:28 +0800

    Create footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import "./App.css";
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar.jsx";
 import CartContextProvider from "./CartContext.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
-    <div>
+    <div className="content">
       <CartContextProvider>
         <Navbar />
         <Outlet />
       </CartContextProvider>
+      <Footer />
     </div>
   );
 }
